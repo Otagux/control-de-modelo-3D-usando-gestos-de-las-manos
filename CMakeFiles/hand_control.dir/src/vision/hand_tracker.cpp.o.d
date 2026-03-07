02ae0fe5d0e01b5ea06c9703c300fd53990db0da
Gustavo <ormejiagustavo@gmail.com>
2026-03-06 20:49:30 -0600

    actualizacion de estructura

---
+++ CMakeFiles/hand_control.dir/src/vision/hand_tracker.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/hand_control.dir/src/vision/hand_tracker.cpp.o: \
+ /home/otagux/VSCODE\ -\ C/control-de-modelo-3D-usando-gestos-de-las-manos/src/vision/hand_tracker.cpp \
+ /usr/include/stdc-predef.h
